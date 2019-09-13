5e5c6c2c6f83680efb3e98864a64ff8b43eabf16
sagalatov sergey <sagalatov@mail.ru>
2019-09-13 13:50:33 +0300

    solved the first task

--- src/index.js
+++ src/index.js
@@ -9,7 +9,14 @@
    delayPromise(3) // вернет promise, который будет разрешен через 3 секунды
  */
 function delayPromise(seconds) {
+ const promise = new Promise((resolve) => {
+    setTimeout(() => {
+      resolve();
+    }, seconds*1000)
+  });
+  return promise;
 }
+
 
 /*
  Задание 2:
@@ -25,6 +32,62 @@ function delayPromise(seconds) {
    loadAndSortTowns().then(towns => console.log(towns)) // должна вывести в консоль отсортированный массив городов
  */
 function loadAndSortTowns() {
+const loadButton = document.querySelector('#LoadButton');
+const container = document.querySelector('#filter-result');
+const formValue = document.querySelector('#filter-input');
+const loading = document.querySelector('#loading-block');
+const fragment = document.createDocumentFragment();
+const allTowns = [];
+loading.innerText = " ";
+
+const xhr = new XMLHttpRequest();
+xhr.open('GET', 'https://raw.githubusercontent.com/smelukov/citiesTest/master/cities.json');
+xhr.responseType = "json";
+xhr.send();
+
+xhr.addEventListener('load', () => {
+  if (xhr.status >= 400) {
+    console.log("Что-то пошло не так");
+  } else {
+    const towns = xhr.response;
+    for (const town of towns) {
+      allTowns.push(town.name);
+    }
+  }
+
+});
+
+formValue.addEventListener('keydown', () => {
+  if (event.code.includes('Key') || event.key == 'Shift') {
+
+    for (const town of allTowns) {
+      if (town.toUpperCase().includes(formValue.value.toUpperCase()) && formValue.value.toUpperCase().length > 1) {
+        fragment.appendChild(createTownDom(town));
+        const promise = new Promise((resolve) => {
+          loading.innerText = "Загрузка...";
+          setTimeout(() => {
+            resolve();
+            container.appendChild(fragment);
+          }, 3000);
+        });
+        promise.then(() => {
+          loading.innerText = " ";
+        });
+      }
+    }
+  } else if (event.key == 'Backspace') {
+    while (container.firstChild) {
+      container.removeChild(container.firstChild);
+    }
+  }
+});
+
+function createTownDom(town) {
+  const div = document.createElement('div');
+  div.textContent = town;
+  return div;
+}
+
 }
 
 export {
